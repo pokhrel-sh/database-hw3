1faca2c66c882c5fc7a2ef1ad78a5ccbb62cab40
Shishir Pokhrel <pokhrel.sh@northeastern.edu>
2024-02-15 14:49:01 -0800

    Fix the tracks on ans1.sql

--- ans1.sql
+++ ans1.sql
@@ -2,6 +2,6 @@ CREATE TABLE "MusicVideo" (
 	"musicVideo_ID"	INTEGER,
 	"track_ID"	INTEGER NOT NULL UNIQUE,
 	"video_Director" TEXT NOT NULL,
-	FOREIGN KEY("track_ID") REFERENCES "t`racks"("TrackId"),
+	FOREIGN KEY("track_ID") REFERENCES "tracks"("TrackId"),
 	PRIMARY KEY("musicVideo_ID")
 );
